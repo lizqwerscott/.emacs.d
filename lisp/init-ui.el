4a11bc8d8c520e8a8bab9bf1615bf5c5d45b939d
lizqwerscott <lizqwerscott@163.com>
2023-01-03 16:33:52 +0800

    change circadian to time

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -135,10 +135,12 @@
 (use-package circadian
   :ensure t
   :config
-  (setq calendar-latitude 37)
-  (setq calendar-longitude 112)
-  (setq circadian-themes '((:sunrise . ef-summer)
-                           (:sunset  . solarized-dark)))
+  ;; (setq calendar-latitude 37)
+  ;; (setq calendar-longitude 112)
+  ;; (setq circadian-themes '((:sunrise . ef-summer)
+  ;;                          (:sunset  . solarized-dark)))
+  (setq circadian-themes '(("8:00" . ef-day)
+                           ("16:00" . solarized-dark)))
   (circadian-setup))
 
 ;;; Background
