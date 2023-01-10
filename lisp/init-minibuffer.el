ace86bc2760516c59da8cbd69fe315263fc3aea2
lizqwerscott <lizqwerscott@163.com>
2023-01-10 12:28:53 +0800

    add init minibuffer

---
+++ lisp/init-minibuffer.el
@@ -0,0 +1,68 @@
+;;; init-minibuffer.el --- About minibuffer config   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  lizqwer scott
+
+;; Author: lizqwer scott <lizqwerscott@gmail.com>
+;;; Commentary:
+
+;;; Code:
+
+;;; Minibuffer
+(use-package vertico
+  :ensure t
+  :init (vertico-mode)
+  :bind
+  (:map vertico-map ("M-TAB" . #'minibuffer-complete)))
+
+(use-package savehist
+  :ensure nil
+  :hook (after-init . savehist-mode)
+  :init (setq enable-recursive-minibuffers t  ; Allow commands in minibuffers
+              history-length 1000
+              savehist-additional-variables '(mark-ring
+                                              global-mark-ring
+                                              search-ring
+                                              regexp-search-ring
+                                              extended-command-history)
+              savehist-autosave-interval 300))
+
+(use-package orderless
+  :ensure t
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package consult
+  :ensure t
+  ;:hook (completion-list-mode . consult-preview-at-point-mode)
+  :init
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+  :config
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key (kbd "M-."))
+  (setq consult-narrow-key "<")
+  (consult-preview-at-point-mode))
+
+(use-package consult-project-extra
+  :ensure t
+  :diminish t)
+
+(use-package marginalia
+  :ensure t
+  :init
+  (marginalia-mode))
+
+(provide 'init-minibuffer)
+;;; init-minibuffer.el ends here.
--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -89,18 +89,6 @@
 ;;   :ensure nil
 ;;   :hook (after-init . save-place-mode))
 
-(use-package savehist
-  :ensure nil
-  :hook (after-init . savehist-mode)
-  :init (setq enable-recursive-minibuffers t  ; Allow commands in minibuffers
-              history-length 1000
-              savehist-additional-variables '(mark-ring
-                                              global-mark-ring
-                                              search-ring
-                                              regexp-search-ring
-                                              extended-command-history)
-              savehist-autosave-interval 300))
-
 (setq load-prefer-newer t)
 
 (setq inhibit-compacting-font-caches nil)
@@ -129,77 +117,34 @@
           #'(lambda ()
               (modify-syntax-entry ?- "w")))
 
-(use-package emacs
-  :init
-  ;; Add prompt indicator to `completing-read-multiple'.
-  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-  (defun crm-indicator (args)
-    (cons (format "[CRM%s] %s"
-                  (replace-regexp-in-string
-                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                   crm-separator)
-                  (car args))
-          (cdr args)))
-  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+;; (use-package emacs
+;;   :init
+;;   ;; Add prompt indicator to `completing-read-multiple'.
+;;   ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+;;   (defun crm-indicator (args)
+;;     (cons (format "[CRM%s] %s"
+;;                   (replace-regexp-in-string
+;;                    "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+;;                    crm-separator)
+;;                   (car args))
+;;           (cdr args)))
+;;   (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
 
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-        '(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+;;   ;; Do not allow the cursor in the minibuffer prompt
+;;   (setq minibuffer-prompt-properties
+;;         '(read-only t cursor-intangible t face minibuffer-prompt))
+;;   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-  ;; Vertico commands are hidden in normal buffers.
-  ;; (setq read-extended-command-predicate
-  ;;       #'command-completion-default-include-p)
+;;   ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+;;   ;; Vertico commands are hidden in normal buffers.
+;;   ;; (setq read-extended-command-predicate
+;;   ;;       #'command-completion-default-include-p)
 
-  ;; Enable recursive minibuffers
-  ;; (setq enable-recursive-minibuffers t)
-  )
+;;   ;; Enable recursive minibuffers
+;;   ;; (setq enable-recursive-minibuffers t)
+;;   )
 
-;;; Minibuffer
-(use-package vertico
-  :ensure t
-  :init (vertico-mode)
-  :bind
-  (:map vertico-map ("M-TAB" . #'minibuffer-complete)))
-
-(use-package orderless
-  :ensure t
-  :init
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles basic partial-completion)))))
-
-(use-package consult
-  :ensure t
-  ;:hook (completion-list-mode . consult-preview-at-point-mode)
-  :init
-  (setq register-preview-delay 0.5
-        register-preview-function #'consult-register-format)
-  (advice-add #'register-preview :override #'consult-register-window)
-
-  (setq xref-show-xrefs-function #'consult-xref
-        xref-show-definitions-function #'consult-xref)
-  :config
-  (consult-customize
-   consult-theme
-   :preview-key '(:debounce 0.2 any)
-   consult-ripgrep consult-git-grep consult-grep
-   consult-bookmark consult-recent-file consult-xref
-   consult--source-bookmark consult--source-recent-file
-   consult--source-project-recent-file
-   :preview-key (kbd "M-."))
-  (setq consult-narrow-key "<")
-  (consult-preview-at-point-mode))
-
-(use-package consult-project-extra
-  :ensure t
-  :diminish t)
-
-(use-package marginalia
-  :ensure t
-  :init
-  (marginalia-mode))
+(require 'init-minibuffer)
 
 ;;; compleations
 (use-package affe
@@ -257,7 +202,6 @@
           path)))
 
 ;;; Tramp
-
 (use-package tramp
   :config
   ;; Enable full-featured Dirvish over TRAMP on certain connections
