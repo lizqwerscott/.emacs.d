f0dd16b489492a67acc9fa17425559c0f760c550
lizqwerscott <lizqwerscott@163.com>
2023-11-04 18:27:57 +0800

    update

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -2,12 +2,7 @@
 (add-hook 'haskell-mode-hook
           #'interactive-haskell-mode)
 
-(add-hook 'haskell-mode-hook
-          #'(lambda ()
-              (haskell-indentation-mode -1)))
-
 ;; (require 'haskell-mode)
 ;; (custom-set-variables '(haskell-process-type 'stack-ghci))
-;; (keymap-set haskell-indentation-mode-map "RET" #'newline-and-indent)
 
 (provide 'init-haskell)
