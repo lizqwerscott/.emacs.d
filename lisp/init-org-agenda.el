b8e008fa6aaa3ed8f78d081512b80640ed12736f
lizqwerscott <lizqwerscott@163.com>
2023-12-11 16:21:09 +0800

    update: add org agenda clock

--- lisp/init-org-agenda.el
+++ lisp/init-org-agenda.el
@@ -1,7 +1,17 @@
 (require 'org-agenda)
 
-(add-to-list 'org-agenda-files
-             "~/Documents/Org/idea.org"
-             "~/Documents/Org/quote.org")
+(add-list-to-list 'org-agenda-files
+                  '("~/Documents/Org/idea.org"
+                    "~/Documents/Org/quote.org"
+                    "~/Documents/Org/tasks.org"))
+
+;; Use a function to decide what to change the state to.
+(setq org-clock-in-switch-to-state #'sodaware/switch-task-on-clock-start)
+
+(defun sodaware/switch-task-on-clock-start (task-state)
+  "Change a task to 'IN-PROGRESS' when TASK-STATE is 'TODO'."
+  (if (string= task-state "TODO")
+      "DOING"
+    task-state))
 
 (provide 'init-org-agenda)
