cce51b5fb1283a78338e9da4543c077ff9cba19c
lizqwerscott <lizqwerscott@163.com>
2023-12-12 23:13:24 +0800

    add custom example

---
+++ custom-example.el
@@ -0,0 +1,19 @@
+;;; custom.el --- user customization file    -*- lexical-binding: t no-byte-compile: t -*-
+;;; Commentary:
+;;;       Add or change the configurations in custom.el, then restart Emacs.
+;;;       Put your own configurations in custom-post.el to override default configurations.
+;;; Code:
+
+(setq user/proxy-host "127.0.0.1")
+
+(custom-set-variables
+ )
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; custom.el ends here
