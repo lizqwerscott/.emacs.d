7252f3447f5a1222941fb032b89901c2697fd123
lizqwerscott <lizqwerscott@163.com>
2023-12-12 16:31:03 +0800

    add telega mnz

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -1,5 +1,54 @@
 (require 'telega)
 (require 'telega-url-shorten)
+(require 'telega-mnz)
+
+(setq telega-mnz-languages
+      '((ada . ada-mode)
+        (awk . awk-mode)
+        (c . c-ts-mode)
+        (clojure . clojure-mode)
+        (cpp . c++-ts-mode)
+        (csharp . csharp-ts-mode)
+        (scheme . scheme-mode)              ; not in language-detection
+        (css . css-mode)
+        (dart . dart-mode)
+        (delphi . delphi-mode)
+        (diff . diff-mode)                  ; not in language-detection
+        (emacslisp . emacs-lisp-mode)
+        (erlang . erlang-mode)
+        (fortran . fortran-mode)
+        (go . go-ts-mode)
+        (groovy . groovy-mode)
+        (haskell . haskell-mode)
+        (html . html-mode)
+        (java . java-mode)
+        (javascript . javascript-mode)
+        (json . json-mode)
+        (kotlin . kotlin-mode)              ; not in language-detection
+        (latex . latex-mode)
+        (lisp . lisp-mode)
+        (lua . lua-mode)
+        (matlab . matlab-mode)
+        (objc . objc-mode)
+        (octave . octave-mode)              ; not in language-detection
+        (org . org-mode)                    ; not in language-detection
+        (outline . outline-mode)            ; not in language-detection
+        (perl . perl-mode)
+        (php . php-mode)
+        (prolog . prolog-mode)
+        (python . python-ts-mode)
+        (r . ess-r-mode)
+        (ruby . ruby-mode)
+        (rust . rust-ts-mode)
+        (scala . scala-mode)
+        (shell . bash-ts-mode)
+        (smalltalk . smalltalk-mode)
+        (sml . sml-mode)
+        (sql . sql-mode)
+        (swift . swift-mode)
+        (visualbasic . visual-basic-mode)
+        (xml . xml-mode)
+        (zig . zig-mode)))
 
 ;;; Config
 (setq telega-proxies
