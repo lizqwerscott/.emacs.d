b09d3f4803b82f8dff5140a75d850e124e723bef
lizqwerscott <lizqwerscott@163.com>
2023-12-17 16:31:01 +0800

    add code stats sync use proxy

--- lisp/init-code-stats.el
+++ lisp/init-code-stats.el
@@ -2,6 +2,19 @@
 (setq code-stats-token
       (auth-source-pick-first-password :host "codestats.net"))
 (add-hook 'prog-mode-hook #'code-stats-mode)
-(run-with-idle-timer 30 t #'code-stats-sync)
-;; (add-hook 'kill-emacs-hook (lambda () (code-stats-sync :wait)))
+
+(defun code-stats-sync-proxy (&optional wait)
+  (let ((url-proxy-services `(("http" . ,(concat
+                                          user/proxy-host
+                                          ":20171"))
+                              ("https" . ,(concat
+                                           user/proxy-host
+                                           ":20171")))))
+    (if wait
+        (code-stats-sync :wait)
+      (code-stats-sync))))
+
+(run-with-idle-timer 30 t
+                     #'code-stats-sync-proxy)
+(add-hook 'kill-emacs-hook (lambda () (code-stats-sync-proxy :wait)))
 (provide 'init-code-stats)
