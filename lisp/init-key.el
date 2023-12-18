ff8d218babeac963c6b913a7b4c5156599afb1a6
lizqwerscott <lizqwerscott@163.com>
2023-12-18 23:14:12 +0800

    fix alpha to 90 and change C-c o to org

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -159,7 +159,8 @@
  (:key "c" :description "Open org capture" :command org-capture :filename "init-org")
  (:key "a" :description "Open org agenda" :command org-agenda :filename "init-org")
  (:key "l" :description "Org store link" :command org-store-link :filename "init-org")
- (:key "s" :description "Org search" :command consult-notes))
+ (:key "s" :description "Org search" :command consult-notes)
+ (:key "r" :description "Org roam" :command one-key-menu-roam))
 
 ;; ;;; ### Watch other window ###
 ;; ;;; --- 滚动其他窗口
