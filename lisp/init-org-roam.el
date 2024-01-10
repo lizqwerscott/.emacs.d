e75a3a98cf8c59915534f4cf993d0626be9909ee
lizqwerscott <lizqwerscott@163.com>
2024-01-10 15:26:32 +0800

    add hugo base dir and org roam export to hugo

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -1,6 +1,9 @@
 (setq org-roam-directory "~/Documents/Org/roam/")
 (org-roam-db-autosync-mode)
 
+(require 'find-lisp)
+(setq org-id-extra-files (find-lisp-find-files org-roam-directory "\.org$"))
+
 (one-key-create-menu
  "Roam"
  '((("f" . "Find roam node") . org-roam-node-find)
