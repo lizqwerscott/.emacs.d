c36a1fd8f652ae3c6b04e5ca2bdfc4c99b02a427
lizqwerscott <lizqwerscott@163.com>
2024-01-18 20:17:04 +0800

    update input

--- lisp/init-input.el
+++ lisp/init-input.el
@@ -47,17 +47,17 @@ Can be used in `rime-disable-predicates' and `rime-inline-predicates'."
 (setq default-input-method "pyim")
 ;; (pyim-default-scheme 'quanpin)
 (pyim-default-scheme 'xiaohe-shuangpin)
-;; (setq pyim-cloudim 'baidu)
+(setq pyim-cloudim 'baidu)
 (setq pyim-page-tooltip 'posframe)
 (setq-default pyim-english-input-switch-functions
               `(
                 ;; pyim-probe-dynamic-english
-                ;; rime-predicate-after-alphabet-char-p
+                rime-predicate-after-alphabet-char-p
                 ;; rime-predicate-after-ascii-char-p
                 ;; rime-predicate-space-after-ascii-p
-                ;; rime-predicate-space-after-cc-p
+                rime-predicate-space-after-cc-p
 
-                ;; pyim-probe-program-mode
+                pyim-probe-program-mode
                 pyim-probe-meow-normal-mode
                 pyim-probe-org-structure-template))
 (setq-default pyim-punctuation-translate-p '(no))
