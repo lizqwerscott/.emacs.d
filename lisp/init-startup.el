550f1f9fee3c5bdd0727cddc437b077600cc377d
lizqwerscott <lizqwerscott@163.com>
2024-02-10 19:34:34 +0800

    feat: add exec form path and update

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -156,5 +156,13 @@
 ;;; vc
 (setq vc-handled-backends '(Git))
 
+;;; exec path from shell
+(setq exec-path-from-shell-variables '("PATH" "MANPATH" "GOROOT" "GOPATH" "EDITOR" "PYTHONPATH"))
+
+;; 设成nil 则不从 .zshrc 读 只从 .zshenv读（可以加快速度，但是需要你将环境变量相关的都放到 .zshenv 中，而非 .zshrc 中）
+(setq exec-path-from-shell-check-startup-files nil) ;
+(setq exec-path-from-shell-arguments '("-l" )) ;remove -i read form .zshenv
+(exec-path-from-shell-initialize)
+
 (provide 'init-startup)
 ;;; init-startup.el ends here.
