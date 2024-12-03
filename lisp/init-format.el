9ca538b0e55b0a040a6c3832f32b2bb0249f7999
lizqwerscott <lizqwerscott@163.com>
2024-12-03 19:45:35 +0800

    feat: use cargo fmt

--- lisp/init-format.el
+++ lisp/init-format.el
@@ -9,4 +9,10 @@
 (setf (alist-get 'c++-ts-mode apheleia-mode-alist)
       'astyle)
 
+(setf (alist-get 'rust-ts-mode apheleia-mode-alist)
+      'cargo-fmt)
+
+(setf (alist-get 'cargo-fmt apheleia-formatters)
+      '("cargo" "fmt"))
+
 (provide 'init-format)
