9911b9f5ff0a2b97c54b9cc5e195fa9ae66ec8b5
lizqwerscott <lizqwerscott@163.com>
2024-12-05 15:09:51 +0800

    feat: add super key for awesome pair command

--- lisp/init-awesome-pair.el
+++ lisp/init-awesome-pair.el
@@ -60,4 +60,8 @@
 (define-key awesome-pair-mode-map (kbd "M-C-n") 'awesome-pair-jump-left)
 (define-key awesome-pair-mode-map (kbd "M-:") 'awesome-pair-jump-out-pair-and-newline)
 
+(define-key awesome-pair-mode-map (kbd "s-C-p") 'awesome-pair-jump-right)
+(define-key awesome-pair-mode-map (kbd "s-C-n") 'awesome-pair-jump-left)
+(define-key awesome-pair-mode-map (kbd "s-:") 'awesome-pair-jump-out-pair-and-newline)
+
 (provide 'init-awesome-pair)
