487af0a6dca68c4fc086220324a6af8ea7d843c0
lizqwerscott <lizqwerscott@163.com>
2025-01-13 14:40:49 +0800

    feat: copilot disable in meow-normal-mode

--- lisp/init-copilot.el
+++ lisp/init-copilot.el
@@ -9,25 +9,13 @@
   (setq copilot-network-proxy
         `(:host ,user/proxy-host :port 20171)))
 
-;;;###autoload
-(defun +lizqwer/toggle-copilot ()
-  "切换copilot"
-  (interactive)
-  (call-interactively 'copilot-mode))
+(add-to-list 'copilot-disable-predicates
+             #'(lambda ()
+                 (symbol-value 'meow-normal-mode)))
 
-;; (global-copilot-mode t)
-
-;; (add-hooks '(python-ts-mode rust-ts-mode c++-ts-mode web-mode bash-ts-mode go-ts-mode csharp-mode csharp-ts-mode)
-;;            #'(lambda ()
-;;                (copilot-mode)
-;;                (copilot-diagnose)))
-
-;; (add-hook 'prog-mode-hook
-;;           #'(lambda ()
-;;               (when (not (or (equal major-mode 'emacs-lisp-mode)
-;;                           (equal major-mode 'lisp-mode)
-;;                           (equal major-mode 'lisp-interaction-mode)
-;;                           (equal major-mode 'sql-mode)))
-;;                 (copilot-mode))))
+(add-hooks '(python-ts-mode rust-ts-mode c++-ts-mode web-mode bash-ts-mode go-ts-mode csharp-mode csharp-ts-mode)
+           #'(lambda ()
+               (copilot-mode)
+               (copilot-diagnose)))
 
 (provide 'init-copilot)
