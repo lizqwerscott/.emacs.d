faa399e256261513f7b87f1bc8be119c48f1f59d
lizqwerscott <lizqwerscott@163.com>
2025-01-13 19:47:54 +0800

    feat: messages-buffer-mode meow normal

--- lisp/init-meow.el
+++ lisp/init-meow.el
@@ -10,6 +10,7 @@
         (helpful-mode . normal)
         (cargo-process-mode . normal)
         (compilation-mode . normal)
+        (messages-buffer-mode . normal)
         (blink-search-mode . insert)
         (color-rg-mode . insert)
         (lsp-bridge-ref-mode . insert)
