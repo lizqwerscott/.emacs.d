7ad25dd635889b7b5993efeb762efc844b58bfb1
lizqwerscott <lizqwerscott@163.com>
2025-02-03 20:18:18 +0800

    refactor: auto save

--- lisp/init-auto-save.el
+++ lisp/init-auto-save.el
@@ -1,12 +1,13 @@
-(add-hook 'prog-mode-hook (lambda ()
-			                (require 'auto-save)
-			                (auto-save-enable)
-			                (setq auto-save-silent t
-                                  auto-save-delete-trailing-whitespace t
-				                  auto-save-idle 1)
-			                ;; (setq auto-save-disable-predicates
-				            ;;       '((lambda ()
-				            ;;           (eq major-mode 'ron-mode))))
-                            ))
+(require 'auto-save)
+(auto-save-enable)
+(setq auto-save-silent t
+      auto-save-delete-trailing-whitespace t
+	  auto-save-idle 1)
+
+(setq auto-save-disable-predicates
+      '((lambda ()
+          (string-suffix-p
+           "gpg"
+           (file-name-extension (buffer-name)) t))))
 
 (provide 'init-auto-save)
