9f05d808f2a491c91c7bd52a75d5b22a13019798
lizqwerscott <lizqwerscott@163.com>
2025-02-08 14:46:19 +0800

    refactor: move some elisp file in lib

--- early-init.el
+++ early-init.el
@@ -11,6 +11,9 @@
   (add-to-list 'load-path
                (expand-file-name
                 (concat user-emacs-directory "lisp")))
+  (add-to-list 'load-path
+               (expand-file-name
+                (concat user-emacs-directory "lib")))
   ;; (setq toggle-debug-on-error t)
   (require 'init-utils)
   (add-subdirs-to-load-path
