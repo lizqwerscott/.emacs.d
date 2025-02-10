31a7402d71883a0793ad857805af4e0ec2025a9c
lizqwerscott <lizqwerscott@163.com>
2025-02-10 15:16:44 +0800

    feat: add separedit in minibuffer, help and helpful mode

--- lisp/init-separedit.el
+++ lisp/init-separedit.el
@@ -1,5 +1,8 @@
 (require 'separedit)
 (keymap-set prog-mode-map "C-c '" #'separedit)
+(keymap-set minibuffer-local-map "C-c '" #'separedit)
+(keymap-set help-mode-map "C-c '" #'separedit)
+(keymap-set helpful-mode-map "C-c '" #'separedit)
 
 (setq separedit-default-mode 'org-mode)
 (setq separedit-remove-trailing-spaces-in-comment t)
