210b14f65494285553d0baf92c2d065dd0222dfd
lizqwerscott <lizqwerscott@163.com>
2025-02-10 15:21:12 +0800

    fix: after helpful mode load

--- lisp/init-separedit.el
+++ lisp/init-separedit.el
@@ -2,7 +2,8 @@
 (keymap-set prog-mode-map "C-c '" #'separedit)
 (keymap-set minibuffer-local-map "C-c '" #'separedit)
 (keymap-set help-mode-map "C-c '" #'separedit)
-(keymap-set helpful-mode-map "C-c '" #'separedit)
+(with-eval-after-load 'helpful
+  (keymap-set helpful-mode-map "C-c '" #'separedit))
 
 (setq separedit-default-mode 'org-mode)
 (setq separedit-remove-trailing-spaces-in-comment t)
