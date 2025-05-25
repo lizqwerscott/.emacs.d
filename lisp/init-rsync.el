59948fbcdeb6bf5033ce915a99d043ef1e2ac3bd
lizqwerscott <lizqwerscott@163.com>
2025-05-25 13:05:02 +0800

    feat: add project remote vterm and eshell

--- lisp/init-rsync.el
+++ lisp/init-rsync.el
@@ -12,25 +12,36 @@
 (add-hook 'prog-mode-hook
           'rsync-project-mode)
 
-(defvar project-remote-connect (make-hash-table :test #'equal))
+(defun project-remote-prefixed-buffer-name (mode)
+  (concat "*"
+          (if-let* ((proj (project-current nil)))
+              (project-name proj)
+            (file-name-nondirectory
+             (directory-file-name default-directory)))
+          "-"
+          "remote"
+          "-"
+          (downcase mode)
+          "*"))
 
 (defun multi-vterm-project-remote ()
   "Connect project remote."
   (interactive)
-  (multi-vterm-project)
-  (let ((root (file-truename (project-root (project-current)))))
-    (when (not (gethash root
-                      project-remote-connect))
-      (puthash root
-               t
-               project-remote-connect)
+  (let* ((root (project-root (project-current t)))
+         (vterm-buffer-name (project-remote-prefixed-buffer-name "vterm"))
+         (vterm-buffer (get-buffer vterm-buffer-name)))
+    (if (and vterm-buffer (not current-prefix-arg))
+        (pop-to-buffer vterm-buffer (append display-buffer--same-window-action
+                                            '((category . comint))))
+      (vterm vterm-buffer-name)
       (when-let* ((remote-config (rsync-project-get-remote-config root))
                   (ssh-config (cl-getf remote-config :ssh-config)))
         (let ((remote-user (cl-getf ssh-config :user))
               (remote-host (cl-getf ssh-config :host))
-              (remote-port (cl-getf ssh-config :port)))
+              (remote-port (cl-getf ssh-config :port))
+              (remote-dir (cl-getf ssh-config :remote-dir)))
           (vterm-send-M-w)
-          (vterm-send-string (format "ssh %s %s"
+          (vterm-send-string (format "ssh -t %s %s 'cd ~/%s && exec $SHELL'"
                                      (if (and remote-user remote-host)
                                          (format "%s@%s"
                                                  remote-user
@@ -40,8 +51,54 @@
                                          (if (not (= 22 remote-port))
                                              (format "-p %s" remote-port)
                                            "")
-                                       ""))
+                                       "")
+                                     remote-dir)
                              t)
           (vterm-send-return))))))
+
+(defun multi-eshell-project-remote ()
+  (interactive)
+  (defvar eshell-buffer-name)
+  (let* ((root (project-root (project-current t)))
+         (eshell-buffer-name (project-remote-prefixed-buffer-name "eshell"))
+         (eshell-buffer (get-buffer eshell-buffer-name)))
+    (if (and eshell-buffer (not current-prefix-arg))
+        (pop-to-buffer eshell-buffer (append display-buffer--same-window-action
+                                             '((category . comint))))
+      (eshell t)
+      (when-let* ((remote-config (rsync-project-get-remote-config root))
+                  (ssh-config (cl-getf remote-config :ssh-config)))
+        (let ((remote-user (cl-getf ssh-config :user))
+              (remote-host (cl-getf ssh-config :host))
+              (remote-port (cl-getf ssh-config :port))
+              (remote-dir (cl-getf ssh-config :remote-dir)))
+          (insert
+           (format "ssh -t %s %s 'cd ~/%s && exec $SHELL'"
+                   (if (and remote-user remote-host)
+                       (format "%s@%s"
+                               remote-user
+                               remote-host)
+                     remote-host)
+                   (if remote-port
+                       (if (not (= 22 remote-port))
+                           (format "-p %s" remote-port)
+                         "")
+                     "")
+                   remote-dir))
+          (eshell-send-input))))))
+
+(transient-define-suffix rsync-project-dispatch-term()
+  (interactive)
+  (call-interactively #'multi-vterm-project-remote))
+
+(transient-define-suffix rsync-project-dispatch-eshell()
+  (interactive)
+  (call-interactively #'multi-eshell-project-remote))
+
+(transient-append-suffix 'rsync-project-dispatch '(-2 -1)
+  ["Mics"
+   :if rsync-project--check
+   ("t" "Open Remote Term" rsync-project-dispatch-term)
+   ("e" "Open Remote Eshell" rsync-project-dispatch-eshell)])
 
 (provide 'init-rsync)
