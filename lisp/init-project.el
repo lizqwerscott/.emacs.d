aa6951febd897d1c86c6aa6cb54c940896a71ebb
Lizqwer scott <lizqwerscott@163.com>
2025-07-15 17:48:21 +0800

    feat: remove find temp project

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -31,48 +31,6 @@
 ;;; config
 (setq project-find-functions '(project-try-vc my/project-try-local))
 
-;;; Find Temp project
-(defvar temp-file-dir "~/temp/" "Set default temp file dir.")
-
-(defun make-lisp-temp (name)
-  (let ((file-name (concat name "/main.lisp")))
-    (make-empty-file file-name)
-    (find-file file-name)))
-
-(defun make-python-temp (name)
-  (let ((file-name (concat name "/main.py")))
-    (make-empty-file file-name)
-    (find-file file-name)))
-
-(defun make-c++-temp (name)
-  (let ((file-name (concat name "/main.cpp")))
-    (make-empty-file file-name)
-    (find-file file-name)))
-
-(defun make-temp-project (args)
-  "Make temp file.
-ARGS is temp project name
-Support Lisp, python, c++."
-  (interactive (list (completing-read "choose one create:"
-                                      `(("Lisp" . 1)
-                                        ("Python" . 2)
-                                        ("C++" . 3))
-                                      nil t "")))
-  (let ((project-name (read-directory-name "make project directory name:"
-                                           temp-file-dir)))
-    (make-directory project-name)
-    (cond ((string= args "Lisp") (make-lisp-temp project-name))
-          ((string= args "Python") (make-python-temp project-name))
-          ((string= args "C++") (make-c++-temp project-name))
-          (t (error "in make-temp-file, the args get another.(%s)" args))))
-  (message args))
-
-(defun find-temp-project ()
-  "Find temp project."
-  (interactive "")
-  (find-file (read-directory-name "find temp project:"
-                                  temp-file-dir)))
-
 ;;; Projection Type
 (defvar projection-project-type-python-uv
   (projection-type
