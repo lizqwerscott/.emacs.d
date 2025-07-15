ae19b22735fa022579a8b65cb38a4edaaed8a7fd
Lizqwer scott <lizqwerscott@163.com>
2025-07-15 17:47:06 +0800

    feat: init-project add head

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,7 +1,32 @@
-(require 'project)
-(require 'projection)
+;;; init-project.el --- config project package       -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2025  lizqwer scott
+
+;; Author: lizqwer scott(require 'project) <lizqwerscott@gmail.com>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'project)
 (require 'project-x)
+(require 'projection)
 
 ;;; config
 (setq project-find-functions '(project-try-vc my/project-try-local))
