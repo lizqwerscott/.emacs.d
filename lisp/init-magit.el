05a8bf7771d13173d2f0782d76fb93825d096415
Lizqwer scott <lizqwerscott@163.com>
2025-08-13 09:22:20 +0800

    fix: use magit-status-goto-initial-section

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -67,7 +67,7 @@ command was called, go to its unstaged changes section."
                        t)
                do (condition-case nil
                       (magit-section-forward)
-                    (error (cl-return (magit-status-goto-initial-section-1))))))))
+                    (error (cl-return (magit-status-goto-initial-section))))))))
 
 ;;;###autoload
 (defun unpackaged/magit-status ()
