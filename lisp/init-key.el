9fe6cd7fd5e910dabe11e5bd679a8509b43d22ab
lizqwerscott <lizqwerscott@163.com>
2025-09-04 22:29:45 +0800

    feat: rebinding some key, and remove notused keybinding

--- lisp/init-key.el
+++ lisp/init-key.el
@@ -1,3 +1,16 @@
+;;; init-key.el --- binding key                      -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(keymap-unset ctl-x-map "b")
+(keymap-unset ctl-x-map "C-k")
+
+(defun ibuffer-refersh ()
+  (interactive)
+  (when-let* ((buffer (get-buffer "*Ibuffer*")))
+    (kill-buffer buffer))
+  (ibuffer))
+
 (global-set-keys
  '(("RET" . newline-and-indent)
    ("S-<return>" . comment-indent-new-line)
@@ -11,9 +24,9 @@
    ("C-s-b" . backward-sexp)
 
    ("C-x C-r" . consult-recent-file)
-   ("C-x C-b" . ibuffer-refersh)))
+   ("C-x C-b" . ibuffer-refersh)
 
-(keymap-unset ctl-x-map "C-k")
+   ("C-x b g" . revert-buffer-quick)))
 
 (with-eval-after-load 'eww
   (keymap-sets eww-mode-map
@@ -38,72 +51,9 @@
        one-key-key-alist))
     `(one-key-create-menu ,title (quote ,one-key-key-alist))))
 
-(defun consult-fd-in-home ()
-  (interactive)
-  (consult-fd "~"))
-
-(lazy-one-key-create-menu
- "Search"
- (:key "l" :description "Search in line in buffer" :command consult-line)
- (:key "L" :description "Search in line in multi buffer" :command consult-line-multi)
- (:key "b" :description "Bookmark" :command consult-bookmark)
- (:key "j" :description "Color rg search symbol" :command color-rg-search-input :filename "init-color-rg")
- (:key "p" :description "Color rg search symbol in project" :command color-rg-search-input-in-project :filename "init-color-rg")
- (:key "r" :description "rg menu" :command rg-menu :filename "init-rg")
- (:key "g" :description "Webjump" :command webjump :filename "init-webjump")
- (:key "f" :description "Search file in home" :command consult-fd-in-home)
- (:key "y" :description "Search YASsnippet" :command consult-yasnippet)
- (:key "w" :description "Search in web" :command consult-omni-multi :filename "init-consult-omni"))
-
-(defun ibuffer-refersh ()
-  (interactive)
-  (when-let* ((buffer (get-buffer "*Ibuffer*")))
-    (kill-buffer buffer))
-  (ibuffer))
-
-(lazy-one-key-create-menu
- "Buffer"
- (:key "b" :description "Switch buffer" :command consult-buffer)
- (:key "B" :description "Switch buffer other window" :command consult-buffer-other-window)
- (:key "s" :description "ibuffer" :command ibuffer-refersh)
- (:key "k" :description "Kill buffer" :command kill-buffer-and-window)
- (:key "T" :description "Switch telega buffers" :command telega-switch-buffer :filename "init-telega")
- (:key "i" :description "Switch telega important chat" :command telega-switch-important-chat :filename "init-telega")
- (:key "t" :description "Switch telega chat" :command telega-chat-with :filename "init-telega")
- (:key "r" :description "Revert buffer" :command revert-buffer-quick)
- (:key "h" :description "bury buffer" :command bury-buffer)
- (:key "l" :description "unbury buffer" :command unbury-buffer))
-
-(lazy-one-key-create-menu
- "FileAction"
- (:key "c" :description "Copy File name" :command +lizqwer/copy-file-name-to-clipboard :filename "init-func")
- (:key "C" :description "Copy File Path" :command +lizqwer/copy-file-path-to-clipboard :filename "init-func")
- (:key "d" :description "Delete this file" :command bufferfile-delete :filename "init-bufferfile")
- (:key "r" :description "Rename this file" :command bufferfile-rename :filename "init-bufferfile")
- (:key "b" :description "Browse this file" :command browse-this-file :filename "init-func"))
-
-(one-key-create-menu
- "File"
- '((("f" . "Find file") . find-file)
-   (("o" . "Find other file") . ff-find-other-file)
-   (("F" . "Find file other window") . find-file-other-window)
-   (("s" . "Fuzzy search") . consult-fd)
-   (("a" . "Action file") . one-key-menu-fileaction)
-   (("r" . "Recent file") . consult-recent-file)
-   (("h" . "Find in main dir") . (lambda ()
-                                   (interactive)
-                                   (ido-find-file-in-dir "~/")))
-   (("p" . "Find in project") . (lambda ()
-                                  (interactive)
-                                  (require 'project)
-                                  (ido-find-file-in-dir "~/MyProject")))
-   (("c" . "Find custom file") . find-custom-file)))
-
 (lazy-one-key-create-menu
  "Useful"
- (:key "t" :description "Sdcv translate" :command sdcv-search-pointer+ :filename "init-sdcv")
  (:key "S" :description "Sudo edit find file" :command sudo-edit-find-file :filename "sudo-edit")
- (:key "e" :description "Toggle sdcv" :command lsp-bridge-toggle-sdcv-helper :filename "init-lsp-bridge")
  (:key "D" :description "Docker" :command docker)
  (:key "p" :description "peek code" :command peek-overlay-dwim :filename "init-peek")
  (:key "d" :description "Devdocs" :command devdocs-lookup)
@@ -224,3 +174,4 @@
       ("<f9>" . gif-screencast-stop))))
 
 (provide 'init-key)
+;;; init-key.el ends here
