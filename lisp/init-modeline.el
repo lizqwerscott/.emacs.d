073471efc84f8f76b4bd733b450eeee2766f7ae1
lizqwerscott <lizqwerscott@163.com>
2025-09-10 22:24:31 +0800

    refactor: modeline

--- lisp/init-modeline.el
+++ lisp/init-modeline.el
@@ -1,50 +1,44 @@
-(defun my-module-process-info ()
-  (format-mode-line mode-line-process))
+;;; init-modeline.el --- init modeline                -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
 
-(defface my-module-process-face
-  '((((background light)) :inherit awesome-tray-blue-face)
-    (t (:inherit awesome-tray-red-face)))
-  "Process module face."
-  :group 'awesome-tray)
+(with-eval-after-load 'awesome-tray
+  (defun my-module-process-info ()
+    (format-mode-line mode-line-process))
 
-(require 'awesome-tray)
+  (defface my-module-process-face
+    '((((background light)) :inherit awesome-tray-blue-face)
+      (t (:inherit awesome-tray-red-face)))
+    "Process module face."
+    :group 'awesome-tray)
 
-(add-to-list 'awesome-tray-module-alist
-             '("process" . (my-module-process-info my-module-process-face)))
+  (add-to-list 'awesome-tray-module-alist
+               '("process" . (my-module-process-info my-module-process-face)))
 
-(setq awesome-tray-date-format "%H:%M")
-;; (setq awesome-tray-active-modules
-;;       (if (string-match-p "Discharging" (shell-command-to-string "acpi"))
-;;           '("meow" "location" "file-path" "buffer-name" "mode-name" "battery" "git" "date")
-;;         '("meow" "location" "file-path" "buffer-name" "mode-name" "git" "date")))
-(setq awesome-tray-active-modules
-      (if (string-match-p "Discharging" (shell-command-to-string "acpi"))
-          '("meow" "location" "buffer-name" "mode-name" "battery" "process" "git" "date")
-        '("meow" "location" "buffer-name" "mode-name" "process" "git" "date")))
-;; (setq awesome-tray-active-modules
-;;       (if (string-match-p "Discharging" (shell-command-to-string "acpi"))
-;;           '("meow" "location" "breadcrumb" "mode-name" "battery" "date")
-;;         '("meow" "location" "breadcrumb" "mode-name" "date")))
+  (setq awesome-tray-date-format "%H:%M")
+  (setq awesome-tray-active-modules
+        (if (string-match-p "Discharging" (shell-command-to-string "acpi"))
+            '("meow" "location" "buffer-name" "mode-name" "battery" "process" "git" "date")
+          '("meow" "location" "buffer-name" "mode-name" "process" "git" "date"))))
 
 (if (and (display-graphic-p) (not user/show-modeline))
-    (progn
-      (awesome-tray-mode))
-  (progn
-    (require 'doom-modeline)
-    (setq doom-modeline-time-icon nil)
-    (setq doom-modeline-buffer-file-name-style
-          'auto)
-    (setq display-time-string-forms
-          '(24-hours ":" minutes " "))
-    (add-hook 'after-init-hook
-              #'doom-modeline-mode)
-    (add-hook 'doom-modeline-mode-hook
-              #'(lambda ()
-                  (display-time-mode)
-                  (when doom-modeline-battery
-                    (display-battery-mode))
-                  (org-count-words-mode)))
-    (add-hook 'org-mode-hook
-              'org-count-words-mode)))
+    (awesome-tray-mode)
+  (require 'doom-modeline)
+  (setq doom-modeline-time-icon nil)
+  (setq doom-modeline-buffer-file-name-style
+        'buffer-name)
+  (setq display-time-string-forms
+        '(24-hours ":" minutes " "))
+  (add-hook 'after-init-hook
+            #'doom-modeline-mode)
+  (add-hook 'doom-modeline-mode-hook
+            #'(lambda ()
+                (display-time-mode)
+                (when doom-modeline-battery
+                  (display-battery-mode))
+                (org-count-words-mode)))
+  (add-hook 'org-mode-hook
+            'org-count-words-mode))
 
 (provide 'init-modeline)
+;;; init-modeline.el ends here
