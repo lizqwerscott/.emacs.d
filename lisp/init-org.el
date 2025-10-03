f1429222208a9cb16e2b86ee9e8cdf16bca1a67e
lizqwerscott <lizqwerscott@163.com>
2025-10-03 14:52:36 +0800

    feat: add hugo template and hugo default setting

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -87,7 +87,9 @@
 (with-eval-after-load 'ox
   (require 'ox-hugo)
   (setq org-hugo-base-dir
-        (file-truename "~/MyProject/website/blog/")))
+        (file-truename "~/MyProject/website/blog/")
+        org-hugo-front-matter-format "yaml"
+        org-hugo-auto-set-lastmod t))
 
 ;;; Org function
 (defun org-export-docx ()
