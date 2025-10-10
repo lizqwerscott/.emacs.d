f370f71fb9dfd1d7e5fb737c233531b5b6917a0c
Lizqwer scott <lizqwerscott@163.com>
2025-10-10 15:18:50 +0800

    refactor: init-format

--- lisp/init-format.el
+++ lisp/init-format.el
@@ -4,18 +4,21 @@
 
 (require 'apheleia)
 
+;; python
 (setf (alist-get 'isort apheleia-formatters)
       '("isort" "--stdout" "-"))
 
 (setf (alist-get 'python-ts-mode apheleia-mode-alist)
       '(isort black))
 
-(setf (alist-get 'rust-ts-mode apheleia-mode-alist)
-      'rustfmt)
-
+;; rust
 (setf (alist-get 'rustfmt apheleia-formatters)
       '("rustfmt" "--quiet" "--emit" "stdout" "--edition" "2024"))
 
+(setf (alist-get 'rust-ts-mode apheleia-mode-alist)
+      'rustfmt)
+
+;; java
 (setf (alist-get 'google-java-format apheleia-formatters)
       '("google-java-format" "--aosp" "-"))
 
