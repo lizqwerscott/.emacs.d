c69d551ae0cf06357ad2a4fcd584bfaf82222866
Lizqwer scott <lizqwerscott@163.com>
2025-10-27 17:50:32 +0800

    feat: add elisp call fucntion in doom-dracula face

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -10,6 +10,10 @@
 
 (setopt elisp-fontify-semantically t)
 
+(when (cl-find 'doom-dracula custom-enabled-themes)
+  (custom-set-faces
+   '(elisp-function ((t (:foreground "SpringGreen4"))))))
+
 (keymap-binds (emacs-lisp-mode-map lisp-interaction-mode-map)
   ("C-c r" . eval-buffer-and-message)
   ("C-c C-p" . ielm)
