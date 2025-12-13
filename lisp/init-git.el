5a8a3fc4677a4b5ac6597386c2587bcfa0a5692d
lizqwer scott <lizqwerscott@163.com>
2025-12-14 00:24:01 +0800

    refactor(git): Move git-related functions to lib-git.el
    - Consolidate git utilities into a dedicated library file
    - Remove init-magit.el and integrate its config into init-git.el

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -24,62 +24,7 @@
 
 ;;; Code:
 
-(defun consult-switch-git-status-buffer ()
-  "Parse git status from an expanded path and switch to a file.
-The completion candidates include the Git status of each file."
-  (interactive)
-  (require 'vc-git)
-  (let ((repo-root (vc-git-root default-directory)))
-	(if (not repo-root)
-		(message "Not inside a Git repository.")
-	  (let* ((expanded-root (expand-file-name repo-root))
-			 (command-to-run (format "git -C %s status --porcelain=v1"
-									 (shell-quote-argument expanded-root)))
-			 (cmd-output (shell-command-to-string command-to-run))
-			 (target-files
-			  (let ((staged-files)
-                    (unstaged-files)
-                    (untracked-files))
-				(dolist (line (split-string cmd-output "\n" t) ;; (nreverse files)
-                              `(("staged" . ,staged-files)
-                                ("unstaged" . ,unstaged-files)
-                                ("untracked" . ,untracked-files)))
-				  (when (> (length line) 3)
-					(let ((status (substring line 0 2))
-						  (path-info (substring line 3)))
-					  ;; Handle rename specially
-					  (if (string-match "^R" status)
-						  (let* ((paths (split-string path-info " -> " t))
-								 (new-path (cadr paths)))
-							(when new-path
-							  (push (cons (format "R %s" new-path) new-path) files)))
-						;; Modified or untracked
-                        (cond
-                         ((string-prefix-p "M" status)
-                          (push path-info staged-files))
-                         ((string-prefix-p " M" status)
-                          (push path-info unstaged-files))
-                         ((string-match "\?\?" status)
-                          (push path-info untracked-files))))))))))
-		(if (not target-files)
-			(message "No modified or renamed files found.")
-		  (let* ((candidates (cl-loop for (gname . items) in target-files
-                                      append (mapcar (lambda (it) (cons it gname)) items)))
-		    	 (selection (consult--read (mapcar #'car candidates)
-                                           :prompt "Switch to buffer (Git modified): "
-                                           :predicate nil
-                                           :require-match t
-                                           :group (lambda (cand transform)
-                                                    (if transform
-                                                        (concat (nerd-icons-icon-for-file cand)
-                                                                " "
-                                                                cand)
-                                                      (alist-get cand candidates nil nil #'equal)))
-                                           :sort nil)))
-		    (when selection
-		      (let ((file-path selection))
-		    	(when file-path
-		    	  (find-file (expand-file-name file-path expanded-root)))))))))))
+(require 'lib-git)
 
 ;;; vc
 (setq vc-handled-backends '(Git)
@@ -116,7 +61,35 @@ The completion candidates include the Git status of each file."
         diff-font-lock-syntax 'hunk-also))
 
 ;;; magit
-(require 'init-magit)
+(with-eval-after-load 'magit
+  (setq magit-diff-refine-hunk t
+        magit-save-repository-buffers nil
+        magit-revision-insert-related-refs nil)
+  (setq magit-delta-hide-plus-minus-markers nil)
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  (setq magit-format-file-function #'magit-format-file-nerd-icons)
+
+  (add-hook 'magit-status-sections-hook
+            #'magit-insert-worktrees t))
+
+(with-eval-after-load 'magit-log
+  ;; Set `magit-log-margin' value in :init as many other variables will be
+  ;; dynamically set based on its value when `magit-log' is loaded.
+  ;; (setq magit-log-margin '(t age magit-log-margin-width t 18)) ;Default value
+  ;; Show the commit ages with 1-char time units
+  ;;   minute->m, hour->h, day->d, week->w, month->M, year->Y
+  ;; Also reduce the author column width to 11 as the author name is being
+  ;; abbreviated below.
+  (setq magit-log-margin '(t age-abbreviated magit-log-margin-width :author 11))
+  (advice-add 'magit-log-format-margin :filter-args #'+magit-log--abbreviate-author))
+
+(add-hook 'magit-mode-hook
+          #'(lambda ()
+              (magit-wip-mode t)
+              (magit-delta-mode t)))
+
+(with-eval-after-load 'magit
+  (require 'forge))
 
 ;;; smerge
 ;;; from https://github.com/alphapapa/unpackaged.el?tab=readme-ov-file#smerge-mode
