e3f0ddfeff97e9481525b62bd46a06ad0de21fe1
lizqwer scott <lizqwerscott@163.com>
2025-12-15 10:05:23 +0800

    feat(org-capture): swap SCHEDULED and DEADLINE order in work template

--- lisp/init-org-capture.el
+++ lisp/init-org-capture.el
@@ -12,7 +12,7 @@
 (push '("l" "LNKS" entry (file+headline "~/Documents/Org/lnks.org" "链接") "* [[%^{link-url}][%^{link-description}]] %^g\n:PROPERTIES:\n:LINK-CREATE-TIME: %T\n:END:\n  %?\n")
       org-capture-templates)
 (push '("t" "任务" entry (file+headline "~/Documents/Org/tasks.org" "任务") "* TODO %^{标题} %^g\nDEADLINE: %^t SCHEDULED: %^t\n  %?\n") org-capture-templates)
-(push '("w" "工作任务" entry (file+headline "~/Documents/Org/tasks.org" "工作任务") "* TODO %^{任务名} :work:\nDEADLINE: %^t SCHEDULED: %^t\n  %?\n" ) org-capture-templates)
+(push '("w" "工作任务" entry (file+headline "~/Documents/Org/tasks.org" "工作任务") "* TODO %^{任务名} :work:\nSCHEDULED: %^t DEADLINE: %^t\n  %?\n" ) org-capture-templates)
 
 (provide 'init-org-capture)
 ;;; init-org-capture.el ends here
