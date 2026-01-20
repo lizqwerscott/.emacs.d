0125f77975801fd13aa749304dcdf45a9b8ccab1
lizqwer scott <lizqwerscott@163.com>
2026-01-20 21:53:46 +0800

    feat(completion): increase max word length for fuzzy scoring
    Set `fussy-max-word-length-to-score` to 5000 to allow longer words
    to be scored during fuzzy completion, improving matching for
    extended queries.

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -36,6 +36,7 @@
 (setopt fussy-score-fn 'fussy-orderless-score-with-flx-rs
         fussy-filter-fn 'fussy-filter-orderless-flex
         fussy-use-cache nil
+        fussy-max-word-length-to-score 5000
         fussy-compare-same-score-fn 'fussy-histlen->strlen<)
 
 (fussy-setup)
